7cf77c738de4926168d260c5d8973742439667d6
donghee426 <jdong3461@gmail.com>
2025-04-14 14:08:54 +0900

    기본 구조

---
+++ src/main/java/com/example/mhbc/MhbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.mhbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mhbc/MhbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mhbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
