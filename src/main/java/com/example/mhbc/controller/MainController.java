ef4445d940fc5d8973ec780f6132f26f50d27b80
minJi1133 <niu0305@naver.com>
2025-04-15 15:44:13 +0900

    0415-3

--- src/main/java/com/example/mhbc/controller/MainController.java
+++ src/main/java/com/example/mhbc/controller/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
     @RequestMapping({"/" , "/home"})
     public String index(Model model){
         System.out.println(">>>>>>>>>>index page<<<<<<<<<<");
-        model.addAttribute("title", "만화방초");
+        model.addAttribute("title", "만화방초");//페이지별 타이틀 설정.(디폴트값==기본 제목)
         return "index";
     }
     @RequestMapping("/login")
