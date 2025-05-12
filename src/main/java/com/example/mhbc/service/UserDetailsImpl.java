0d5cca633de1a7886acaefd7e238ba346012f289
minJi1133 <niu0305@naver.com>
2025-05-12 16:15:04 +0900

    0512 로그인 유저 정보
    userDetails / UserDetailService / SecurityConfig 수정

--- src/main/java/com/example/mhbc/service/UserDetailsImpl.java
+++ src/main/java/com/example/mhbc/service/UserDetailsImpl.java
@@ -9,13 +9,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Data /*어노테이션을 통해 기본적인 getter, setter, equals, hashCode, toString 자동 생성*/
 public class UserDetailsImpl implements UserDetails {  // Spring Security의 UserDetails 인터페이스 구현
 
-   private MemberEntity member;
+   private Optional<MemberEntity> member;
 
-    public UserDetailsImpl(MemberEntity member) {
+    public UserDetailsImpl(Optional<MemberEntity> member) {
         this.member = member;
     }
 
@@ -25,16 +26,16 @@ public class UserDetailsImpl implements UserDetails {  // Spring Security의 Use
         // 여기서는 "ROLE_USER" 권한을 가진 SimpleGrantedAuthority 객체를 리스트로 반환
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
 
-        //일반 유저 권한
-        if(member.getGrade() == 1){
-            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        }
+        //유저 권한
+        member.ifPresent(m -> {
+            if (m.getGrade() == 1) {
+                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+            } else if (m.getGrade() == 10) {
+                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+                authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+            }
+        });
 
-        //관리자 권한
-        if(member.getGrade() == 10){
-            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-        }
         return authorities;
     }
 
@@ -42,20 +43,23 @@ public class UserDetailsImpl implements UserDetails {  // Spring Security의 Use
     public String getPassword() {
         // 사용자의 비밀번호를 반환하는 메서드
         System.out.println("----getPassword----");
-        return member.getPwd();
+        return member.map(MemberEntity::getPwd)
+                .orElseThrow(() -> new IllegalStateException("비밀번호가 없습니다"));
     }
 
     @Override
     public String getUsername() {
         // 사용자의 아이디(Username)를 반환하는 메서드
         System.out.println("----getUsername----");
-        return member.getUserid();
+        return member.map(MemberEntity::getUserid)
+                .orElseThrow(() -> new IllegalStateException("아이디가 없습니다"));
     }
 
     public Long getIdx() {
         // 사용자의 아이디(Username)를 반환하는 메서드
         System.out.println("----getUsername----");
-        return member.getIdx();
+        return member.map(MemberEntity::getIdx)
+                .orElseThrow(() -> new IllegalStateException("유저(idx)가 없습니다"));
     }
 
 
