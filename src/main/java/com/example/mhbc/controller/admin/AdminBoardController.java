870d0174033a1adeae4cbff4abe9cc9878692db2
minJi1133 <niu0305@naver.com>
2025-05-12 14:53:59 +0900

    0512

--- src/main/java/com/example/mhbc/controller/admin/AdminBoardController.java
+++ src/main/java/com/example/mhbc/controller/admin/AdminBoardController.java
@@ -1,4 +1,25 @@
 package com.example.mhbc.controller.admin;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/board")
 public class AdminBoardController {
+
+    @RequestMapping("/list")
+    public String board_page(Model model){
+
+
+
+        model.addAttribute("webtitle", "게시물관리 | 리스트");
+
+        return "/admin/board/list";
+    }
+
 }
