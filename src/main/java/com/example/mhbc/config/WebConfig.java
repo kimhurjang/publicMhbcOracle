a5b1226f069764529ba6e630103573eed88fa392
dong <niu0305@naver.com>
2025-05-13 09:53:15 +0900

    0513 이벤트 이미지 경로 수정

--- src/main/java/com/example/mhbc/config/WebConfig.java
+++ src/main/java/com/example/mhbc/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.mhbc.config;
 
+import com.fasterxml.jackson.core.Base64Variant;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/data/**") // URL 경로
-                .addResourceLocations("file:Z:/public/data/"); // 실제 폴더 경로
+                .addResourceLocations("file:C:/Users/YJ/Documents/GitHub/public/data/"); // 실제 폴더 경로(192.168.0.190)
+                /*.addResourceLocations("file:Z:/public/data/");//로컬*/
     }
 }
