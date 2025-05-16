ff5a0259f80ee4cc88498513b9a716ea870c87d4
donghee426 <jdong3461@gmail.com>
2025-05-16 16:24:24 +0900

    메인 디자인 수정_텍스트 애니메이션 효과

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,17 +1,74 @@
-let scrollTop = document.querySelector('.scroll-top');
+/*-------------- main visual text animation  --------------*/
+const elementsManhwa = document.querySelectorAll('.name .letter');
+const subtitlePhrases = document.querySelectorAll('.subtitle .phrase');
+const totalManhwa = elementsManhwa.length;
 
-function toggleScrollTop() {
-  if (scrollTop) {
-    window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-  }
-}
-scrollTop.addEventListener('click', (e) => {
-  e.preventDefault();
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
+function resetLetters() {
+  elementsManhwa.forEach(el => {
+    el.style.opacity = 0;
   });
-});
 
-window.addEventListener('load', toggleScrollTop);
-document.addEventListener('scroll', toggleScrollTop);
+  subtitlePhrases.forEach(el => {
+    el.style.opacity = 0;
+  });
+}
+
+function fadeInLetters() {
+  resetLetters();
+
+  anime({
+    targets: elementsManhwa,
+    opacity: 1,
+    scale: 1,
+    easing: 'easeInOutQuad',
+    duration: 800,
+    delay: (el, i) => i * 150,
+    complete: () => {
+      setTimeout(() => fadeInSubtitle(), 500);
+    }
+  });
+}
+
+function fadeInSubtitle() {
+  anime({
+    targets: subtitlePhrases,
+    opacity: 1,
+    scale: 1,
+    easing: 'easeInOutQuad',
+    duration: 800,
+    delay: (el, i) => i * 500, // 덩어리별로 보여지게
+    complete: () => {
+      setTimeout(() => fadeOutSubtitle(), 5000);
+    }
+  });
+}
+
+function fadeOutSubtitle() {
+  anime({
+    targets: subtitlePhrases,
+    opacity: 0,
+    scale: 0.8,
+    easing: 'easeInOutQuad',
+    duration: 800,
+    delay: (el, i) => (subtitlePhrases.length - i - 1) * 300, // 역순 덩어리씩 지워짐
+    complete: () => {
+      fadeOutLetters();
+    }
+  });
+}
+
+function fadeOutLetters() {
+  anime({
+    targets: elementsManhwa,
+    opacity: 0,
+    scale: 0.8,
+    easing: 'easeInOutQuad',
+    duration: 800,
+    delay: (el, i) => (totalManhwa - i - 1) * 150,
+    complete: () => {
+      setTimeout(() => fadeInLetters(), 2000);
+    }
+  });
+}
+
+fadeInLetters();
