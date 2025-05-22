0a7c0368bcf152adc1658aef3fb4cd66972110a1
donghee426 <jdong3461@gmail.com>
2025-05-22 17:04:01 +0900

    장소소개, 대관안내 페이지 작성

--- src/main/resources/static/js/comm.js
+++ src/main/resources/static/js/comm.js
@@ -5,7 +5,7 @@ function toggleScrollTop() {
     window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
   }
 }
-scrollTop?.addEventListener('click', (e) => {
+scrollTop.addEventListener('click', (e) => {
   e.preventDefault();
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
@@ -23,3 +23,39 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 window.addEventListener('load', () => AOS.refresh());
+
+window.addEventListener("load", function () {
+  console.log(document.querySelectorAll(".ani_box").length);
+  console.log(typeof ScrollTrigger);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  // ani_box가 렌더링되었는지 감지
+  const observer = new MutationObserver(() => {
+    const boxes = gsap.utils.toArray(".ani_box");
+    if (boxes.length > 0) {
+      boxes.forEach((el, i) => {
+        gsap.from(el, {
+          scrollTrigger: {
+            trigger: el,
+            start: "top 90%",
+            toggleActions: "play none none none"
+          },
+          duration: 1.5,
+          rotateY: 90,
+          opacity: 0,
+          x: 100,
+          ease: "power3.out",
+          delay: i * 0.1
+        });
+      });
+      ScrollTrigger.refresh(); // 꼭 필요
+      observer.disconnect(); // 더 감시 안 해도 됨
+    }
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+});
